5df94a5dd65293ba630311b5cb6ba3945a7b140b
Are Sløtte <aretov@live.com>
2021-11-08 12:00:26 +0100

    changes

--- pythonWorkshop/workshop.py
+++ pythonWorkshop/workshop.py
@@ -5,4 +5,18 @@ def frontend():
     username = ""
     while (username == ""):
         print("-- USER FRONTEND --")
-        
+        print("Please choose an option:\n 1. register\n "
+              "2. Login\n 3. Exit:")
+        ch = get_int_from_user("option: ", True)
+    pass
+
+
+def get_int_from_user(msg, needed):
+    while True:
+        numStr = input(msg)
+        if (numStr == "" and not needed):
+            return None;
+        try:
+            return int(numStr)
+        except:
+            print("Please provide an integer or leave blank.");
